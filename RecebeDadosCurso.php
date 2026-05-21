790218ce687e26fc5ed264d01cbe00aa39206caf
ra2557007 <luca.bressan@unesp.br>
2026-05-21 11:09:49 -0300

    sistema feito

---
+++ RecebeDadosCurso.php
@@ -0,0 +1,53 @@
+<html>
+    <head>
+        <style>
+            * {
+                font-size: 40px;
+                align-items: center;
+            }
+        th, td {
+            border: 1px solid #000;
+            padding: 8px;
+            text-align: left;
+            min-width: 23vw;
+            max-width: 23vw;
+        }
+        tbody > tr:nth-child(2n) {
+            background-color:lightgray;
+        }
+        </style>
+    </head>
+    <table cellspacing="0">
+        <thead>
+            <th>ID</th>
+            <th>TITULO</th>
+            <th>DESCRICAO</th>
+            <th>VALOR</th>
+            
+        </thead>
+        <tbody>
+    <?php 
+        include("util.php");
+        $conn = connect("bd_Luca_Cursos");
+
+        $valueFilterLowest = $_POST['low-filter']; //Recebe os dados do POST
+        $valueFilterHighest = $_POST['high-filter'];
+        echo "Maior que: " . $valueFilterLowest. "<br>Menor que: " . $valueFilterHighest;
+
+        $varSQL = "SELECT * FROM cursos WHERE valor >= :valormin AND valor <= :valormax"; //Filtra o SQL
+        $select =  $conn->prepare($varSQL);
+        $select -> bindParam(":valormin",$valueFilterLowest);
+        $select -> bindParam(":valormax",$valueFilterHighest);
+        $select -> execute();
+
+        while ($linha = $select -> fetch()) {
+            $id = $linha['id'];
+            $titulo = $linha['titulo'];
+            $desc = $linha['descricao'];
+            $valor = $linha['valor'];
+            echo "<tr><td>" . $id . "</td><td>" . $titulo . "</td><td>" . $desc . "</td><td>" . $valor . "</td>";
+        }
+        ?>
+        </tbody>
+    </table>
+</html>
